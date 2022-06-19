0a5d918a061b531940440980a711da340679bb6e
morozanna <a.moroz201648@gmail.com>
2022-06-19 17:40:19 +0200

    changed class Ant

--- ant.rb
+++ ant.rb
@@ -13,51 +13,18 @@ class Leaf < Field
     def to_s
         "L"
     end
-    
+end
+
 class Ant
     
-    def initialize(curr_position)
-        @curr_position = curr_position
-        @last_position = []
-        @hasLeaf = false
-        @break = 0
-    end
-    
-    def set_position(new_position)
-        @last_position = @curr_position
-        @curr_position = new_position
+    def initialize(x, y)
+        @x = x
+        @y= y
+        @hasLeaf = nil
     end
 
-    def take_leaf()
-        @hasLeaf = true
-        @break = 5
-    end
-
-    def leave_leaf()
-        @hasLeaf = false
-        @break = 5
-    end
-
-    def move(new_position)
-        @last_position = @curr_position
-        @curr_position = new_position
-        @break -= 1
-    end
-
-    def get_curr_position()
-        @curr_position
-    end
-
-    def get_last_position()
-        @last_position
-    end
-
-    def get_has_leaf()
-        @hasLeaf
-    end
-
-    def get_break()
-        @break
+    def to_s
+        @hasLeaf.nil? ? "A*" : "A" 
     end
 end
 
